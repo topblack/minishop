dee04dd036efabab9ea5010449ed932a8c9fb6d2
lqin-pki <leon.qin@perkinelmer.com>
2017-08-27 18:16:49 +0800

    expose 8080 instead.

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,4 +4,4 @@ CONTAINER=minishop
 docker stop $CONTAINER
 docker rm $CONTAINER
 docker rmi $IMAGE
-docker run -d -p 80:80 --restart=always --name $CONTAINER $IMAGE
+docker run -d -p 8080:80 --restart=always --name $CONTAINER $IMAGE
