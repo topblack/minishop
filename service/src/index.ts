d46478994d7ddb450a4a5e42b112983d78476d14
lqin-pki <leon.qin@perkinelmer.com>
2017-10-17 23:56:07 +0800

    Adding wechat get handler.

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 
+const handleWeChatGet = (req: any, res: any) => {
+  res.send(req.params.echostr);
+}
+
 app.use('/', express.static('ui'));
 
-app.get('/wx', this.handleWeChatGet);
+app.get('/wx', handleWeChatGet);
 
 app.listen(8080, function () {
   console.info('Listening the port 8080...');
 });
 
-const handleWeChatGet = (req: any, res: any) => {
-  res.send(req.params.echostr);
-}
