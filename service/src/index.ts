bfe1ec563c3ac64835304d73303986a85949338b
lqin-pki <leon.qin@perkinelmer.com>
2017-10-18 00:39:48 +0800

    Fix the parameter issue.

--- service/src/index.ts
+++ service/src/index.ts
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 
 const handleWeChatGet = (req: any, res: any) => {
-  res.send(req.params.echostr);
+  console.info(req.query.echostr);
+  res.send(req.query.echostr);
 }
 
-app.get('/wx', handleWeChatGet).use('/', express.static('ui'));
+app.get('/wx', handleWeChatGet)
+  .use('/', express.static('ui'));
 
 app.listen(8080, function () {
   console.info('Listening the port 8080...');
