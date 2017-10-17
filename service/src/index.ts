3c647061f998f771ff404ec0fd04afe7bbe9be26
lqin-pki <leon.qin@perkinelmer.com>
2017-10-17 23:55:19 +0800

    Add wx verify handler.

--- service/src/index.ts
+++ service/src/index.ts
@@ -3,6 +3,12 @@ const app = express()
 
 app.use('/', express.static('ui'));
 
+app.get('/wx', this.handleWeChatGet);
+
 app.listen(8080, function () {
   console.info('Listening the port 8080...');
 });
+
+const handleWeChatGet = (req: any, res: any) => {
+  res.send(req.params.echostr);
+}
