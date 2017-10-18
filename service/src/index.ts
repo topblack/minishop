e86d4c18c12392f908ff97cf8251b0e09d60422b
lqin-pki <leon.qin@perkinelmer.com>
2017-10-18 21:56:49 +0800

    Fix the routing on detail and payment.

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+const routes = require('express').Router();
 
 const handleWeChatGet = (req: any, res: any) => {
-  console.info(req.query.echostr);
   res.send(req.query.echostr);
 }
 
+const handleGetDetail = (req: any, res: any) => {
+  res.sendFile(path.join(__dirname, '../ui/index.html'));
+}
+
+const handleGetPayment = (req: any, res: any) => {
+  res.sendFile(path.join(__dirname, '../ui/index.html'));
+}
+
 app.get('/wx', handleWeChatGet)
-  .use('/', express.static('ui'));
+  .use('/', express.static('ui'))
+  .get('/detail/:actId', handleGetDetail)
+  .get('/payment/:actId', handleGetPayment);
 
 app.listen(8080, function () {
   console.info('Listening the port 8080...');
